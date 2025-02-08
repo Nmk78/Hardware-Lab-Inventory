29cd6e99fabe8f26bab96262210cc3d8f118f9d7
Nmk78 <naymyokhant78@gmail.com>
2025-02-08 22:45:45 +0630

    fixing prisma error in vercel

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,35 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+// const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['error'],
-    // log: ['query', 'info', 'warn', 'error'],
-  })
+// export const prisma =
+//   globalForPrisma.prisma ||
+//   new PrismaClient({
+//     log: ['error'],
+//     // log: ['query', 'info', 'warn', 'error'],
+//   })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-// prisma.$on('query', (e: { query: string; params: string; duration: string }) => {
-//   console.log('Query: ' + e.query)
-//   console.log('Params: ' + e.params)
-//   console.log('Duration: ' + e.duration + 'ms')
-// })
+// // prisma.$on('query', (e: { query: string; params: string; duration: string }) => {
+// //   console.log('Query: ' + e.query)
+// //   console.log('Params: ' + e.params)
+// //   console.log('Duration: ' + e.duration + 'ms')
+// // })
 
-// export { prisma }
+// // export { prisma }
+// @ts-nocheck
+import { PrismaClient } from '../../prisma/generated/client';
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
