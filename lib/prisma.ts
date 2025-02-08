554bb16d0037347ebb5a58c3cefe1db769aa6dd0
Nmk78 <naymyokhant78@gmail.com>
2025-02-08 23:04:52 +0630

    fixing prisma error in vercel by adding pre_build script.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,36 +1,21 @@
-// import { PrismaClient } from '@prisma/client'
-
-// const globalForPrisma = global as unknown as { prisma: PrismaClient }
-
-// export const prisma =
-//   globalForPrisma.prisma ||
-//   new PrismaClient({
-//     log: ['error'],
-//     // log: ['query', 'info', 'warn', 'error'],
-//   })
-
-// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-// // prisma.$on('query', (e: { query: string; params: string; duration: string }) => {
-// //   console.log('Query: ' + e.query)
-// //   console.log('Params: ' + e.params)
-// //   console.log('Duration: ' + e.duration + 'ms')
-// // })
-
-// // export { prisma }
-
-
 import { PrismaClient } from '@prisma/client'
 
-let prisma: PrismaClient;
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['error'],
+    // log: ['query', 'info', 'warn', 'error'],
+  })
 
-export default prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+// prisma.$on('query', (e: { query: string; params: string; duration: string }) => {
+//   console.log('Query: ' + e.query)
+//   console.log('Params: ' + e.params)
+//   console.log('Duration: ' + e.duration + 'ms')
+// })
+
+// export { prisma }
+
